3a4a03cd2815f1155f2b710b0966b398027225b8
Yuri Ognyanov <80990845+YuriOgnyanov@users.noreply.github.com>
2022-11-22 17:13:46 +0200

    Rectangle area
    calculation Area of Rectangle

---
+++ FirstStepsInProgramming/03. Rectangle Area/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+
+            var rectangleArea = a * b;
+
+            Console.WriteLine(rectangleArea);
+        }
+    }
+}
