bb78c8235c15699517034568cdb3ad1629725692
Yuri Ognyanov <80990845+YuriOgnyanov@users.noreply.github.com>
2022-11-22 17:25:17 +0200

    Greeting by Name

---
+++ FirstStepsInProgramming/05. Greeting by Name/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _05._Greeting_by_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
