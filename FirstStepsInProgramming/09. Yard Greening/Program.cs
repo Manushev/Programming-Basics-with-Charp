d3a65f76a6d8a70f0a2caa0ee87d065c2430a97b
Yuri Ognyanov <80990845+YuriOgnyanov@users.noreply.github.com>
2022-11-22 18:25:44 +0200

    Yard Greening

---
+++ FirstStepsInProgramming/09. Yard Greening/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09._Yard_Greening
+{
+    internal class Program
+    {
+        const double PricePerSquareMeter = 7.61;
+        const double Discount = 0.18;
+        static void Main(string[] args)
+        {
+            var squareMeters = double.Parse(Console.ReadLine());
+
+            var landscapingPrice = squareMeters * PricePerSquareMeter;
+
+            var landscapingDiscount = landscapingPrice * Discount;
+
+            var totalPrice = landscapingPrice - landscapingDiscount;
+
+            Console.WriteLine($"The final price is: {totalPrice} lv.");
+            Console.WriteLine($"The discount is: {landscapingDiscount} lv.");
+        }
+    }
+}
