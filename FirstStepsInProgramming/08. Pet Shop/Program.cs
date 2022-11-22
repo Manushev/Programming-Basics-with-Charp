a025c9797599a4efbc1e74cf698e10458c20336b
Yuri Ognyanov <80990845+YuriOgnyanov@users.noreply.github.com>
2022-11-22 18:03:00 +0200

    Pet Shop

---
+++ FirstStepsInProgramming/08. Pet Shop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08._Pet_Shop
+{
+    internal class Program
+    {
+        const double DogFood = 2.50;
+        const double CatFood = 4;
+        static void Main(string[] args)
+        {
+            var numOfDogFood = int.Parse(Console.ReadLine());
+            var numOfCatFood = int.Parse(Console.ReadLine());
+
+            var dogFoodPrice = numOfDogFood * DogFood;
+            var catFoodPrice = numOfCatFood * CatFood;
+
+            var totalSum = dogFoodPrice + catFoodPrice;
+
+            Console.WriteLine($"{totalSum} lv.");
+        }
+    }
+}
