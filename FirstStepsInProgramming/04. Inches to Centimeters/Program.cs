e4fd5d88b885f156850ffe285e83ef113134d360
Yuri Ognyanov <80990845+YuriOgnyanov@users.noreply.github.com>
2022-11-22 17:20:41 +0200

    Inches converter

---
+++ FirstStepsInProgramming/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        const double centimetersPerInch = 2.54;
+        static void Main(string[] args)
+        {
+            var inches = double.Parse(Console.ReadLine());
+
+            var centimeters = inches * centimetersPerInch;
+
+            Console.WriteLine(centimeters);
+        }
+    }
+}
