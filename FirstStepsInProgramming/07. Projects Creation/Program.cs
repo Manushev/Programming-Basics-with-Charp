11f50659e54a3f8c518a4a26d1e87e32c83f3e1d
Yuri Ognyanov <80990845+YuriOgnyanov@users.noreply.github.com>
2022-11-22 17:48:15 +0200

    Projects Creation

---
+++ FirstStepsInProgramming/07. Projects Creation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07._Projects_Creation
+{
+    internal class Program
+    {
+        const int hourPerProject = 3;
+        static void Main(string[] args)
+        {
+            var nameOfArchitect = Console.ReadLine();
+            var numOfProjects = int.Parse(Console.ReadLine());
+
+            var needHours = numOfProjects * hourPerProject;
+
+            Console.WriteLine($"The architect {nameOfArchitect} will need {needHours} hours to complete {numOfProjects} project/s.");
+        }
+    }
+}
