bcd9e52b79cd2478cd2d2d10601352c37552586c
Yuri Ognyanov <80990845+YuriOgnyanov@users.noreply.github.com>
2022-11-23 16:27:48 +0200

    USD to BGN

---
+++ First Steps In Coding - Exercise/01. USD to BGN/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._USD_to_BGN
+{
+    internal class Program
+    {
+        const double BgnFixedRatePerUsd = 1.79549;
+        static void Main(string[] args)
+        {
+            var usd = double.Parse(Console.ReadLine());
+
+            var bgn = usd * BgnFixedRatePerUsd;
+
+            Console.WriteLine(bgn);
+        }
+    }
+}
