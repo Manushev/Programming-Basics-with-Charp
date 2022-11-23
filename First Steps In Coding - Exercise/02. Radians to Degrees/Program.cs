3a2e8a4ed6ad87c6f30fb2c32942129331c90742
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-11-23 16:42:17 +0200

    Radians to Degrees

---
+++ First Steps In Coding - Exercise/02. Radians to Degrees/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var radians = double.Parse(Console.ReadLine());
+
+            var degrees = radians * 180 / Math.PI;
+
+            Console.WriteLine(degrees);
+        }
+    }
+}
