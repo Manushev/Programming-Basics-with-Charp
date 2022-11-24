1dbc384f47f1255a3e0650a1502593d671fbc4be
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-11-24 03:56:39 +0200

    Deposit Calculator

---
+++ First Steps In Coding - Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var deposit = double.Parse(Console.ReadLine());
+            var term = int.Parse(Console.ReadLine());
+            var annualInterestRate = double.Parse(Console.ReadLine());
+
+            //сума = депозирана сума + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
+            var total = deposit + term * ((deposit * (annualInterestRate / 100)) / 12);
+
+            Console.WriteLine(total);
+        }
+    }
+}
