e0bd77513ef68e8d1cb97edeac7e26791b4ecac6
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-11-25 17:16:39 +0200

    Vacation Books List

---
+++ First Steps In Coding - Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfPages = int.Parse(Console.ReadLine());
+            var readedPages = int.Parse(Console.ReadLine());
+            var days = int.Parse(Console.ReadLine());
+
+            var readTime = numberOfPages / readedPages;
+            var timePeDay = readTime / days;
+
+            Console.WriteLine(timePeDay);
+        }
+    }
+}
