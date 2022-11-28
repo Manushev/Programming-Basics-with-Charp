80707ae532ec76b856d29bb59d61a1cbfb695171
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-11-28 18:35:19 +0200

    Basketball Equipment

---
+++ First Steps In Coding - Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var basketballFee = int.Parse(Console.ReadLine());
+
+            //Equipment
+            var sneakersPrice = basketballFee - (basketballFee * 0.40);
+            var clothingPrice = sneakersPrice - (sneakersPrice * 0.20);
+            ;
+            var ballPrice = clothingPrice / 4;
+            var accessories = ballPrice / 5;
+
+            var totalExpenses = basketballFee + sneakersPrice + clothingPrice + ballPrice + accessories;
+
+            Console.WriteLine(totalExpenses);
+        }
+    }
+}
