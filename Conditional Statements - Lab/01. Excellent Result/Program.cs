6e31f7aba3a01cdb8bc4642f2e7b0ae4be0a8bb7
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-11-30 17:44:22 +0200

    Conditional Statements Lab - Excellent Result task

---
+++ Conditional Statements - Lab/01. Excellent Result/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Excellent_Result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var evaluation = double.Parse(Console.ReadLine());
+
+            if (evaluation >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
