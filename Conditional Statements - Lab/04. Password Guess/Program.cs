a4d3f46c8ceb0292ad70e3f75a8b0a2bcab0826e
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-11-30 18:06:33 +0200

    Password Guess

---
+++ Conditional Statements - Lab/04. Password Guess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Password_Guess
+{
+    internal class Program
+    {
+        const string Password = "s3cr3t!P@ssw0rd";
+        static void Main(string[] args)
+        {
+            var passwordGuess = Console.ReadLine();
+
+            if (passwordGuess == Password)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
