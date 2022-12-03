f24c790abe9994dc39febe36ea9c2de36640ea6f
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-04 01:00:50 +0200

    Sum Seconds

---
+++ ConsoleApp1Conditional Statements - Exercise/01. Sum Seconds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTime = int.Parse(Console.ReadLine());
+            var secondTime = int.Parse(Console.ReadLine());
+            var thirdTime = int.Parse(Console.ReadLine());
+
+            var totalTimeInSeconds = firstTime + secondTime + thirdTime;
+            
+            var minutes = totalTimeInSeconds / 60;
+            var seconds = totalTimeInSeconds % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+                return;
+            }
+            Console.WriteLine($"{minutes}:{seconds}");
+            
+        }
+    }
+}
