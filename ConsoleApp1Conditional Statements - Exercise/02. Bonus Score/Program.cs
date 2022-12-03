8bb5712989a52e4f2c6d73987f8cc27f56952be8
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-04 01:20:51 +0200

    Bonus Score

---
+++ ConsoleApp1Conditional Statements - Exercise/02. Bonus Score/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._Bonus_Score
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = int.Parse(Console.ReadLine());
+
+            var bonusPoints = 0.0;
+
+            if (points <= 100)
+            {
+                bonusPoints += 5;
+                
+            }
+            else if (points > 100 && points <= 1000)
+            {
+                bonusPoints += points * 0.20;
+            }
+            else
+            {
+                bonusPoints += points * 0.10;
+            }
+
+            if (points % 2 == 0)
+            {
+                bonusPoints += 1;
+            }
+            if (points % 10 == 5)
+            {
+                bonusPoints += 2;
+            }
+
+
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(points + bonusPoints);
+        }
+    }
+}
