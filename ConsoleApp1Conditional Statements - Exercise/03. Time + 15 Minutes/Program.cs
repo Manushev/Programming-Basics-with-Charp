e6ea9c8f009065c3510911b31956e69bb9d826c9
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-04 01:53:08 +0200

    Time + 15 Minutes

---
+++ ConsoleApp1Conditional Statements - Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03._Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hour = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+
+            if (hour >= 0 && hour <= 23 && minutes >= 0 && minutes <= 59)
+            {
+                minutes += 15;
+
+                if (minutes > 59)
+                {
+                    hour += 1;
+                    minutes -= 60;
+                }
+                if (hour > 23)
+                {
+                    hour = 0;
+                }
+                if (minutes < 10)
+                {
+                    Console.WriteLine($"{hour}:0{minutes}");
+                    return;
+                }
+
+                Console.WriteLine($"{hour}:{minutes}");
+            }
+        }
+    }
+}
