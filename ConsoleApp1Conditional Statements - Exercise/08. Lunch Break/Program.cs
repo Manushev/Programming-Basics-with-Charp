e0ff3b58db724ac0e29ea09196f75635287498cd
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-04 16:16:55 +0200

    Lunch Break

---
+++ ConsoleApp1Conditional Statements - Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filmName = Console.ReadLine();
+            var episode = int.Parse(Console.ReadLine());
+            var breakTime = int.Parse(Console.ReadLine());
+            
+            double lunch = breakTime * 0.125;
+            double rest = breakTime * 0.25;
+            double timeForRest = Math.Ceiling(episode + lunch + rest);
+            if (timeForRest <= breakTime)
+            {
+                Console.WriteLine($"You have enough time to watch {name} and left with {breakTime - timeForRest} minutes free time.");
+            }
+            else 
+            {
+                Console.WriteLine($"You don't have enough time to watch {name}, you need {timeForRest - breakTime} more minutes.");
+            }
+        }
+    }
+}
