5c0f5d161e1d2142166f9e4d40700815debd0d5e
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-04 15:14:19 +0200

    World Swimming Record

---
+++ ConsoleApp1Conditional Statements - Exercise/06. Swimming Record/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Swimming_Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var recordInSeconds = double.Parse(Console.ReadLine());
+            var distance = double.Parse(Console.ReadLine());
+            var timeInSeconds = double.Parse(Console.ReadLine());
+
+            var seconds = distance * timeInSeconds;
+            
+            var delayTimesInSeconds = Math.Floor(distance / 15) * 12.5;
+
+            var totalTime = seconds + delayTimesInSeconds;
+
+            if (recordInSeconds > totalTime)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalTime - recordInSeconds:F2} seconds slower.");
+            }
+        }
+    }
+}
