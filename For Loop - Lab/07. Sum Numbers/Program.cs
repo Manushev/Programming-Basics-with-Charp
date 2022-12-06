74bd20ded8001e9c2c03c82a3011c7d5243c8fc3
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-06 11:30:30 +0200

    Sum Numbers and Number sequence

---
+++ For Loop - Lab/07. Sum Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersOfDigits = int.Parse(Console.ReadLine());
+
+            var sum = 0;
+
+            for (int i = 0; i < numbersOfDigits; i++)
+            {
+                var digit = int.Parse(Console.ReadLine());
+                sum += digit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Number_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersOfDigfits = int.Parse(Console.ReadLine());
+
+            var array = new int[numbersOfDigfits];
+
+            for (int i = 0; i < numbersOfDigfits; i++)
+            {
+                var digit = int.Parse(Console.ReadLine());
+
+                array[i] = digit;
+            }
+
+            var minDigit = array.Min();
+            var maxDigit = array.Max();
+
+            Console.WriteLine($"Max number: {maxDigit}");
+            Console.WriteLine($"Min number: {minDigit}");
+        }
+    }
+}
