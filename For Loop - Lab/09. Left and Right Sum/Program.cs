630013d29188064b99ee0567646807a2ca5ec682
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-06 11:44:53 +0200

    Left and Right Sum

---
+++ For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfDigits = int.Parse(Console.ReadLine());
+
+            var leftSum = 0;
+            var rightSum = 0;
+
+            for (int i = 1; i <= numberOfDigits * 2; i++)
+            {
+                var digit = int.Parse(Console.ReadLine());
+
+                if (i <= numberOfDigits)
+                {
+                    leftSum += digit;
+                }
+                else
+                {
+                    rightSum += digit;
+                }
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {leftSum - rightSum}");
+            }
+        }
+    }
+}
