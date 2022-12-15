b4ac3a35f551790b6044fd5cfe0e51f8c3fb6665
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-15 02:12:53 +0200

    Animal Type

---
+++ Conditional Statements Advanced - Lab/03. Animal Type/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03._Animal_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameOfAnimal = Console.ReadLine();
+
+            var typeOfAnimal = string.Empty;
+
+            switch (nameOfAnimal)
+            {
+                case "dog":
+                    typeOfAnimal = "mammal";
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    typeOfAnimal = "reptile";
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+
+            Console.WriteLine(typeOfAnimal);
+        }
+    }
+}
