86217b18f54275cf8d5e52eec4ee67393ed0bd8b
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-15 22:32:18 +0200

    Working Hours

---
+++ Conditional Statements Advanced - Lab/07.Working Hours/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hour = int.Parse(Console.ReadLine());
+            var day = Console.ReadLine();
+
+            var output = string.Empty;
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    if (hour >= 10 && hour <= 18)
+                    {
+                        output = "open";
+                    }
+                    else
+                    {
+                        output = "closed";
+                    }
+                    break;
+                case "Sunday":
+                    output = "closed";
+                    break;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
