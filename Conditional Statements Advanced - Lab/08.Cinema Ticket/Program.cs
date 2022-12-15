4afecec0b18c7c5cf7fc93ceec660e33dd3a936b
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-15 22:41:27 +0200

    Cinema Ticket

---
+++ Conditional Statements Advanced - Lab/08.Cinema Ticket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08.Cinema_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = Console.ReadLine();
+
+            var tichetPrice = 0;
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Friday":
+                    tichetPrice = 12;
+                    break;
+                case "Wednesday":
+                case "Thursday":
+                    tichetPrice = 14;
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    tichetPrice = 16;
+                    break;
+            }
+
+            Console.WriteLine(tichetPrice);
+        }
+    }
+}
