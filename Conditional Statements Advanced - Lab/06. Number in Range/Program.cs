7849525baa8318b682e9620946fc69dc5609da7a
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-15 22:17:46 +0200

    Number in Range

---
+++ Conditional Statements Advanced - Lab/06. Number in Range/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Number_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var output = string.Empty;
+
+            if (number != 0 && number >= -100 && number <= 100)
+            {
+                output = "Yes";
+            }
+            else
+            {
+                output = "No";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
