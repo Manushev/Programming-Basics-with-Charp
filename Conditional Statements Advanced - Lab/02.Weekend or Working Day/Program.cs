871797daec919d03439e14100b605772fd032e6e
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-15 02:06:57 +0200

    Weekend or Working Day

---
+++ Conditional Statements Advanced - Lab/02.Weekend or Working Day/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02.Weekend_or_Working_Day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayOfWeek = Console.ReadLine();
+
+            var workingOrWeekendDay = string.Empty;
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                    workingOrWeekendDay = "Working day";
+                    break;
+                case "Tuesday":
+                    workingOrWeekendDay = "Working day";
+                    break;
+                case "Wednesday":
+                    workingOrWeekendDay = "Working day";
+                    break;
+                case "Thursday":
+                    workingOrWeekendDay = "Working day";
+                    break;
+                case "Friday":
+                    workingOrWeekendDay = "Working day";
+                    break;
+                case "Saturday":
+                    workingOrWeekendDay = "Weekend";
+                    break;
+                case "Sunday":
+                    workingOrWeekendDay = "Weekend";
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+
+            Console.WriteLine(workingOrWeekendDay);
+        }
+    }
+}
