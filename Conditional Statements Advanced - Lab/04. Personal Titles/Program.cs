fcaffdf70555a778f1e972e7cff9e3426736afea
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-15 02:22:57 +0200

    Personal Titles

---
+++ Conditional Statements Advanced - Lab/04. Personal Titles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04._Personal_Titles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = double.Parse(Console.ReadLine());
+
+            var gender = char.Parse(Console.ReadLine());
+
+            switch (gender)
+            {
+                case 'm':
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Master");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mr.");
+                    }
+                    break;
+                case 'f':
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Miss");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ms.");
+                    }
+                    break;
+            }
+        }
+    }
+}
