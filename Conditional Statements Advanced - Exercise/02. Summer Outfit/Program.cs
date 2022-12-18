36df272ae50949670f7410701b093f62847ebdf3
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-19 00:24:26 +0200

    Summer Outfit

---
+++ Conditional Statements Advanced - Exercise/02. Summer Outfit/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _02._Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var degrees = int.Parse(Console.ReadLine());
+            var timeOfDay = Console.ReadLine();
+
+            var outFit = string.Empty;
+            var shoes = string.Empty;
+
+            if (10 <= degrees && degrees <=18)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    outFit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (timeOfDay == "Afternoon")
+                {
+                    outFit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (timeOfDay == "Evening")
+                {
+                    outFit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else if (degrees <= 24)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    outFit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (timeOfDay == "Afternoon")
+                {
+                    outFit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfDay == "Evening")
+                {
+                    outFit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else
+            {
+                if (timeOfDay == "Morning")
+                {
+                    outFit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfDay == "Afternoon")
+                {
+                    outFit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                else if (timeOfDay == "Evening")
+                {
+                    outFit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+
+            Console.WriteLine($"It's {degrees} degrees, get your {outFit} and {shoes}.");
+        }
+    }
+}
