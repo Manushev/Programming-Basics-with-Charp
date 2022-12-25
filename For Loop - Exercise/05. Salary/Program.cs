6c1a054486facb3e8416092add3aa7302c313fc8
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-25 13:36:40 +0200

    Salary

---
+++ For Loop - Exercise/05. Salary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05._Salary
+{
+    internal class Program
+    {
+        const int FacebookFine = 150;
+        const int Instagramfine = 100;
+        const int RedditFine = 50;
+        static void Main(string[] args)
+        {
+            var numOfOpenTabs = int.Parse(Console.ReadLine());
+            var salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numOfOpenTabs; i++)
+            {
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+
+                var nameOfWebSite = Console.ReadLine();
+                                
+                if (nameOfWebSite == "Facebook")
+                {
+                    salary -= FacebookFine;
+                }
+                else if (nameOfWebSite == "Instagram")
+                {
+                    salary -= Instagramfine;
+                }
+                else if (nameOfWebSite == "Reddit")
+                {
+                    salary -= RedditFine;
+                }
+            }
+
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+            
+        }
+    }
+}
