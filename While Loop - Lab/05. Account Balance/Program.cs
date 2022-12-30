0bb1d9532fbfe533318fb7523a6f416198aee366
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-30 22:30:47 +0200

    Account Balance

---
+++ While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var availableMoney = 0.0;
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "NoMoreMoney")
+                {
+                    break;
+                }
+
+                var paymant = double.Parse(cmd);
+
+                if (paymant < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Increase: {paymant:F2}");
+                    availableMoney += paymant;
+                }
+            }
+
+            Console.WriteLine($"Total: {availableMoney:F2}");
+        }
+    }
+}
