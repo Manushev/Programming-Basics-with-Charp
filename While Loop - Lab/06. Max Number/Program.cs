2ba12eb8ed091bb0db24a780a0172ad1a8ec7954
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-30 22:36:40 +0200

    Max Number

---
+++ While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxNumber = int.MinValue;
+
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "Stop")
+                {
+                    break;
+                }
+
+                var number = int.Parse(cmd);
+
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
