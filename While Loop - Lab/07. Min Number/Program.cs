0978d105d8bf1253d4958fda9bca62fa12b548f6
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-30 22:39:04 +0200

    Min Number

---
+++ While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var minNumber = int.MaxValue;
+
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "Stop")
+                {
+                    break;
+                }
+
+                var number = int.Parse(cmd);
+
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+
+            Console.WriteLine(minNumber);
+        }
+    }
+}
