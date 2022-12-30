8f8fc98e1c87e8db193a77ebde8f891bb930d7a6
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-30 19:46:53 +0200

    Read Text

---
+++ While Loop - Lab/01. Read Text/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Read_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = Console.ReadLine();
+
+            Console.WriteLine();
+
+            while (cmd != "Stop")
+            {
+                Console.WriteLine(cmd);
+                cmd = Console.ReadLine();
+
+            }
+        }
+    }
+}
