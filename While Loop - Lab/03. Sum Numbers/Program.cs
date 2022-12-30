24567bd828054c739a72f478deb4fc6ea30c9b32
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-30 20:15:22 +0200

    Sum Numbers

---
+++ While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            while (true)
+            {
+                var currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+
+                if (sum >= num)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
