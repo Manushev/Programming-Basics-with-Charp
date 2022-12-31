21fc4f21a5aa987f40fa4d815b6cf5d146768570
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2022-12-31 11:01:25 +0200

    Old Books

---
+++ While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = Console.ReadLine();
+            var searchBook = Console.ReadLine();
+            var bookCounter = 0;
+            
+            while (searchBook != "No More Books")
+            {
+                if (searchBook == book)
+                {
+                    Console.WriteLine($"You checked {bookCounter} books and found it.");
+                    break;
+                }
+
+                searchBook = Console.ReadLine();
+                bookCounter++;
+            }
+
+            if (book != searchBook)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {bookCounter} books.");
+            }
+        }
+    }
+}
