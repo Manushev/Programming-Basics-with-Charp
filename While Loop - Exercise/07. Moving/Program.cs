fa739b6c7b12f1077f312b2d13b169f94864441c
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2023-01-03 16:15:20 +0200

    Moving

---
+++ While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var widthOfSpace = int.Parse(Console.ReadLine());
+            var lengthOfSpace = int.Parse(Console.ReadLine());
+            var heightOfSpace = int.Parse(Console.ReadLine());
+
+            var totalFreeSpace =  widthOfSpace * lengthOfSpace * heightOfSpace;
+
+            while (totalFreeSpace > 0)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "Done")
+                {
+                    break;
+                }
+
+                var numOfBox = int.Parse(cmd);
+                totalFreeSpace -= numOfBox;
+            }
+
+            if (totalFreeSpace > 0)
+            {
+                Console.WriteLine($"{totalFreeSpace} Cubic meters left.");
+            }
+                
+            if (totalFreeSpace <= 0)
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(totalFreeSpace)} Cubic meters more.");
+            }
+        }
+    }
+}
