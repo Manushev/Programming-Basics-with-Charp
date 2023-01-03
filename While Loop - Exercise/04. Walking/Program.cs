f6c562919d17405d242c11c92c106d16077ba648
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2023-01-03 15:25:40 +0200

    Walking

---
+++ While Loop - Exercise/04. Walking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04._Walking
+{
+    internal class Program
+    {
+        const int PurposeStepPerDay = 10000;
+        static void Main(string[] args)
+        {
+
+            var currentSteps = 0;
+            var isGoingHome = false;
+
+            while (currentSteps <= PurposeStepPerDay && !isGoingHome)
+            {
+                var cmd = Console.ReadLine();
+
+                if (cmd == "Going home")
+                {
+                    cmd = Console.ReadLine();
+                    isGoingHome = true;
+                }
+
+                var steps = int.Parse(cmd);
+
+                currentSteps += steps;
+            }
+
+            if (currentSteps >= PurposeStepPerDay)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{Math.Abs(PurposeStepPerDay - currentSteps)} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Abs(PurposeStepPerDay - currentSteps)} more steps to reach goal.");
+            }
+        }
+    }
+}
