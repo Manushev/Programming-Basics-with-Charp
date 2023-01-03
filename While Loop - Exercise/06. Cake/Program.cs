227c4e17e3f644088654d6eaa7eccf381cb4ddd3
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2023-01-03 15:47:02 +0200

    Cake

---
+++ While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var widthCake = int.Parse(Console.ReadLine());
+            var lengthCake = int.Parse(Console.ReadLine());
+
+            var pieceOfCake = widthCake * lengthCake;
+
+            while (pieceOfCake >= 0)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "STOP")
+                {
+                    Console.WriteLine($"{pieceOfCake} pieces are left.");
+                    break;
+                }
+
+                var pieces = int.Parse(cmd);
+
+                pieceOfCake -= pieces;
+            }
+
+            if (pieceOfCake <= 0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(pieceOfCake)} pieces more.");
+            }
+        }
+    }
+}
