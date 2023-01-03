03cda9be643a5c13acd84d6fcc002727dbc850fa
Yuri Ognyanov <118821156+Manushev@users.noreply.github.com>
2023-01-03 16:27:45 +0200

    Multiplication Table

---
+++ Nested Loops - Lab/02. Multiplication Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int f = 1; f <= 10; f++)
+                {
+                    Console.WriteLine($"{i} * {f} = {i * f}");
+                }
+            }
+        }
+    }
+}
